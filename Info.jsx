1bf7168f1901e659ee7076f68aef83c9e3d3b63f
Shreya Chowdhury <1930152@kiit.ac.in>
2021-06-27 18:23:35 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript & React.js</h1>
+      <p>This is a Basic web dev React Js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
